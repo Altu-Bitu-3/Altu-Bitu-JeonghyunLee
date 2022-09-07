5a440d4fac5daee39f3487b93044f09f99d979a6
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-09-07 13:09:02 +0900

    정렬 과제

---
+++ Sorting/BOJ1758.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N; //사람의 수
+	cin >> N;
+
+	vector<int> original_tip(N); //각 사람이 주려고 하는 팁
+	vector<int> final_tip(N); //강호가 받는 팁
+	long long tip_all = 0; //범위 때문에 int 형 쓰면 오버플로우 일어난다
+
+	for (int i = 0; i < N; i++) {
+		cin >> original_tip[i];
+	}
+
+
+	//팁 많이 주려고 하는 사람부터 받기 -> 내림차순 정렬
+	//처음에는 음수이면 팁 안 주므로 적게 팁 주는 사람이 앞에 오게끔 오름차순 정렬을 했는데,
+	//그렇게 하면 많이 주는 사람이 팁을 못 줄 수가 있어서 최댓값이 아님
+	sort(original_tip.begin(), original_tip.end(), greater<>());
+
+	for (int i = 0; i < N; i++) {
+		final_tip[i] = original_tip[i] - i; //i는 (등수 - 1)
+		if(final_tip[i] > 0)
+			tip_all += final_tip[i];
+	}
+
+	cout << tip_all;
+}
