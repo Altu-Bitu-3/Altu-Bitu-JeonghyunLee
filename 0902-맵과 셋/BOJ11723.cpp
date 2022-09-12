b6e126d08d3fbb649ceb14271e787716d8edba00
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-09-12 10:18:28 +0900

    [정렬] 9월 12일

---
+++ 0902-맵과 셋/BOJ11723.cpp
@@ -0,0 +1,59 @@
+//처음에는 set을 이용했지만 이 문제는 set을 이용한다면 비효율적(시간초과)
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arr(21);
+
+void solution(string str) {
+	if (str == "all") {
+		for (int i = 1; i <= 20; i++) {
+			arr[i] = 1;
+		}
+	}
+	else if (str == "empty") {
+		for (int i = 1; i <= 20; i++) {
+			arr[i] = 0;
+		}
+	}
+	else {
+		int num;
+		cin >> num;
+
+		if (str == "add") {
+			arr[num] = 1;
+		}
+		else if (str == "remove") {
+			arr[num] = 0;
+		}
+		else if (str == "check") {
+			cout << arr[num] << "\n";
+		}
+		else if (str == "toggle") {
+			if (arr[num] == 1) {
+				arr[num] = 0;
+			}
+			else if (arr[num] == 0)
+				arr[num] = 1;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int M; //연산의 수
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		string str;
+		cin >> str;
+		solution(str);
+	}
+
+	return 0;
+}
---
+++ 0902-맵과 셋/BOJ14425.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	set<string> s;
+
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		s.insert(str);
+	}
+
+	int ans = 0;
+	for (int i = 0; i < M; i++) {
+		string str;
+		cin >> str;
+		if (s.find(str) != s.end())
+			ans++;
+	}
+
+	cout << ans;
+
+	return 0;
+}
