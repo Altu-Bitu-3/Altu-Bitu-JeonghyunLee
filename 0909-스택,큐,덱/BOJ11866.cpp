5d48251450f014d7c575b827c82789ab78925ede
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-09-21 00:48:03 +0900

    [스택큐덱] 9월 21일 - Update

--- 0909-스택,큐,덱/BOJ11866.cpp
+++ 0909-스택,큐,덱/BOJ11866.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	int n, k;
-	cin >> n >> k;
+vector<int> josephus(int n, int k) {
+    queue<int> q;
+    vector<int> v;
 
-	queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
 
-	for (int i = 1; i <= n; i++) {
-		q.push(i);
-	}
-
-    cout << "<";
-	int j = 1;
-	while (!q.empty()) {
+    int j = 1;
+    while (!q.empty()) {
         if (j % k != 0) { //제거 차례 아님
             //앞에 있던 수 큐 뒤로 보내기 (원 다음 칸으로 이동)
-            int temp = q.front();
+            q.push(q.front());
             q.pop();
-            q.push(temp);
         }
         else { //제거 차례
             int ans = q.front();
             q.pop(); //제거
             if (q.empty()) {
-                cout << ans << ">";
-                return 0;
+                v.push_back(ans);
+                return v;
             }
-            cout << ans << ", ";
+            v.push_back(ans);
         }
-		j++;
-	}
+        j++;
+    }
+    return v;
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+    vector<int> v = josephus(n, k);
+
+    cout << "<";
+    for (auto iter = v.begin(); iter != v.end() - 1; iter++) {
+        cout << *iter << ", ";
+    }
+    cout << v[v.size()-1] << ">";
+
+    return 0;
 }
