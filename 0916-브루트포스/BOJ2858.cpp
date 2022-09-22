3a082e301d8c33bf409d35a54ee9657e4a71d0c2
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-09-22 09:41:53 +0900

    [브루트포스] 9월 22일

---
+++ 0916-브루트포스/BOJ2858.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// 2(L + W - 2) = R
+// (L-2)*(W-2) = B
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int r, b;
+	cin >> r >> b;
+
+	for (int l = 3; ; l++) {
+		for (int w = 3; w <= l; w++) {
+			if (2 * (l + w - 2) == r && (l - 2) * (w - 2) == b) {
+				cout << l << " " << w;
+				return 0;
+			}
+		}
+	}
+
+}
