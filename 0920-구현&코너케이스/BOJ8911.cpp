11665806deba9e0c0b069a014cba91dcb0e532cc
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-09-30 16:32:09 +0900

    [구현&코너케이스] 9월 30일 update

--- 0920-구현&코너케이스/BOJ8911.cpp
+++ 0920-구현&코너케이스/BOJ8911.cpp
@@ -6,51 +6,44 @@ using namespace std;
 int turtle(string s) {
 	int x = 0;
 	int y = 0;
-	int maxX = 0, minX = 0, maxY = 0, minY = 0;
+	int max_x = 0, min_x = 0, max_y = 0, min_y = 0;
+
+	//북 서 남 동
+	int dx[4] = { 0, -1, 0, 1 };
+	int dy[4] = { 1, 0, -1, 0 };
+
 	int direction = 0; //0: 북, 1: 서, 2: 남, 3: 동
 
 	for (int i = 0; i < s.length(); i++) {
 		switch (s[i])
 		{
 		case 'F':
-			if (direction == 0)
-				y++;
-			else if (direction == 1)
-				x--;
-			else if (direction == 2)
-				y--;
-			else if (direction == 3)
-				x++;
+			x += dx[direction];
+			y += dy[direction];
 
-			if (x > maxX)
-				maxX = x;
-			if (x < minX)
-				minX = x;
-			if (y > maxY)
-				maxY = y;
-			if (y < minY)
-				minY = y;
+			if (x > max_x)
+				max_x = x;
+			if (x < min_x)
+				min_x = x;
+			if (y > max_y)
+				max_y = y;
+			if (y < min_y)
+				min_y = y;
 
 			break;
 
 		case 'B':
-			if (direction == 0)
-				y--;
-			else if (direction == 1)
-				x++;
-			else if (direction == 2)
-				y++;
-			else if (direction == 3)
-				x--;
+			x -= dx[direction];
+			y -= dy[direction];
 
-			if (x > maxX)
-				maxX = x;
-			if (x < minX)
-				minX = x;
-			if (y > maxY)
-				maxY = y;
-			if (y < minY)
-				minY = y;
+			if (x > max_x)
+				max_x = x;
+			if (x < min_x)
+				min_x = x;
+			if (y > max_y)
+				max_y = y;
+			if (y < min_y)
+				min_y = y;
 
 			break;
 
@@ -68,7 +61,7 @@ int turtle(string s) {
 		}
 	}
 
-	return (maxX - minX) * (maxY - minY);
+	return (max_x - min_x) * (max_y - min_y);
 }
 
 int main() {
